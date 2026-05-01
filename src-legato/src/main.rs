8fc179402e4b56d057e6e85957a7c7aa252828c8
Luke <lukeweston1234@gmail.com>
2026-05-01 20:29:57 +0200

    sine example

--- src-legato/src/main.rs
+++ src-legato/src/main.rs
@@ -1,3 +1,32 @@
+use legato::{
+    builder::{LegatoBuilder, Unconfigured},
+    config::Config,
+    interface::AudioInterface,
+    ports::PortBuilder,
+};
+
 fn main() {
-    println!("Hello, world!");
+    let graph = std::fs::read_to_string("../.legato").unwrap();
+
+    let config = Config {
+        sample_rate: 44_100,
+        block_size: 4096,
+        channels: 2,
+        rt_capacity: 0,
+    };
+
+    let ports = PortBuilder::default().audio_out(2).build();
+
+    let (app, _) = LegatoBuilder::<Unconfigured>::new(config, ports).build_dsl(&graph);
+
+    #[cfg(target_os = "macos")]
+    let host = cpal::host_from_id(cpal::HostId::CoreAudio).expect("JACK host not available");
+
+    #[cfg(target_os = "linux")]
+    let host = cpal::host_from_id(cpal::HostId::Jack).expect("JACK host not available");
+
+    AudioInterface::builder(&host, config)
+        .build(app)
+        .expect("Failed to start audio")
+        .run_forever();
 }
